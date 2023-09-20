b763f9915f951ed59de1b360d2d6f5ec615f2bff
jang0hyo-bi <jhb942266@gmail.com>
2023-09-20 03:05:44 +0000

    chap2 python file added

--- p61.py
+++ p61.py
@@ -1,3 +1,19 @@
-print('Trust Fund Buddy')
+print('          Trust Fund Buddy          ')
+print("Totals your monthly spending so that your trust fund dosen't run out")
+print("<and you're forced to get a real job>.")
+
+print("Please enter the requested, monthly costs. Since you're rich, ignore pennies and use only dolloar amounts.")
+
+car = int(input("Lamborghini Tune-Ups: "))
+rent = int(input("Manhattan Apartment: "))
+jet = int(input("Prinate Jet Rental: "))
+gifts = int(input("Gifsts: "))
+food = int(input("Dinin Out: "))
+staff = int(input("Staff(buters,chef,m driver, assistant): "))
+guru =int(input("personal Guru and Coach: "))
+gmaes = int(input("Computer Games: "))
+
+total = car+rent+jet+gifts+food+staff+guru+games
+print("Grand Total: ", total)
 
 
