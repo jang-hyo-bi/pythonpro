a141d73b8f23c81173e11de08893d2cd4cb4d1b8
jang0hyo-bi <jhb942266@gmail.com>
2023-10-24 05:45:21 +0000

    2023 10 24

---
+++ chap6/prob2.py
@@ -0,0 +1,33 @@
+print("\tHigh Scores Keeper")
+
+scores = []
+while True:
+    
+    print("")
+    print("\t0 - Quit")
+    print("\t1 - List")
+    print("\t2 - Add a Score")
+    print("")
+    
+    num_input = int(input("Choice: "))
+
+    if num_input == 0:
+        break
+
+    if num_input == 2:
+        name_input = input("What is the player's name?: ")
+        score_input = int(input("What score did the player get?:"))
+        scores.append((score_input, name_input))
+    
+    if num_input == 1:
+        print("High Scores")
+        print("")
+        print("NAME\tSCORE")
+        
+        scores.sort()
+        scores.reverse()
+        
+        for entry in scores:
+            score_input, name_input = entry
+            print(name_input, "\t", score_input)
+
